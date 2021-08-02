3c50a7396bed51076b39ee2367a9dac945c33be1
otherzoe <zoe@keyte.me>
2021-08-02 15:58:54 -0500

    nestedplayRound  function

--- scripting.js
+++ scripting.js
@@ -5,48 +5,23 @@ function computerPlay() {
     let randomOption = options[Math.floor(Math.random() * options.length)];
     return randomOption;
 }
-
-function playRound(playerSelection, computerSelection) {
-    if (
-        (playerSelection === 'paper')
-        && (computerSelection === 'rock')
-    ) {
-        return 'You win! Paper beats rock!';
-    } else if (
-        (playerSelection === 'paper')
-        && (computerSelection === 'scissors')
-    ) {
-        return 'You lose! Scissors beats paper!';
-    } else if (
-        (playerSelection === 'rock')
-        && (computerSelection === 'paper')
-    ) {
-        return 'You lose! Paper beats rock!';
-    } else if (
-        (playerSelection === 'rock')
-        && (computerSelection === 'scissors')
-    ) {
-        return 'You win! Rock beats scissors!';
-    } else if (
-        (playerSelection === 'scissors')
-        && (computerSelection === 'rock')
-    ) {
-        return 'You lose! Rock beats scissors!';
-    } else if (
-        (playerSelection === 'scissors')
-        && (computerSelection === 'paper')
-    ) {
-        return 'You win! Paper beats scissors!';
-    } else {
-        return 'You tied. Play again!';
-    }
-}
+let playerWon = 0;
+let computerWon = 0;
 
 function game() {
-let playerSelection = prompt('Rock, paper, or scissors?');
+
+    let computerWon;
+//    const computerSelection = computerPlay();
+    const computerSelection = 'rock';
+
+    /* correct code, hidden for debugging 
+    let playerSelection = prompt('Rock, paper, or scissors?');
     playerSelection = playerSelection.toLowerCase();    
     console.log(playerSelection);
+    */
     
+    playerSelection = 'paper';
+
     // if a nonviable option is entered, show an error msg
     if (
     (playerSelection !== 'rock')
@@ -57,7 +32,45 @@ let playerSelection = prompt('Rock, paper, or scissors?');
         prompt('Rock, paper, or scissors?');
     }
 
-const computerSelection = computerPlay();
+    function playRound(playerSelection, computerSelection) {
 
+        if (
+            (playerSelection === 'paper')
+            && (computerSelection === 'rock')
+        ) {
+            console.log('You win! Paper beats rock!');
+            playerWon = ++playerWon;
+        } else if (
+            (playerSelection === 'paper')
+            && (computerSelection === 'scissors')
+        ) {
+            console.log('You lose! Scissors beats paper!');
+        } else if (
+            (playerSelection === 'rock')
+            && (computerSelection === 'paper')
+        ) {
+            console.log('You lose! Paper beats rock!');
+        } else if (
+            (playerSelection === 'rock')
+            && (computerSelection === 'scissors')
+        ) {
+            console.log('You win! Rock beats scissors!');
+        } else if (
+            (playerSelection === 'scissors')
+            && (computerSelection === 'rock')
+        ) {
+            console.log('You lose! Rock beats scissors!');
+        } else if (
+            (playerSelection === 'scissors')
+            && (computerSelection === 'paper')
+        ) {
+            console.log('You win! Paper beats scissors!');
+        } else {
+            console.log('You tied. Play again!');
+        }
+    }
+
+    playRound();
+    console.log(playRound(playerSelection, computerSelection));
 }
 game();
