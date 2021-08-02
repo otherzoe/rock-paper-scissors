8027fd8a40e00080a828c01842fcf878cd713ea2
otherzoe <zoe@keyte.me>
2021-08-02 14:57:40 -0500

    if statements added for each scenario, bugfixing in progress

--- scripting.js
+++ scripting.js
@@ -6,3 +6,44 @@ function computerPlay() {
     // log for debugging purposes
     console.log(randomOption);
 }
+
+function playRound(playerSelection, computerSelection) {
+    if (
+        (playerSelection === 'paper')
+        && (computerSelection === 'rock')
+    ) {
+        return 'You win! Paper beats rock!';
+    } else if (
+        (playerSelection === 'paper')
+        && (computerSelection === 'scissors')
+    ) {
+        return 'You lose! Scissors beats paper!';
+    } else if (
+        (playerSelection === 'rock')
+        && (computerSelection === 'paper')
+    ) {
+        return 'You lose! Paper beats rock!';
+    } else if (
+        (playerSelection === 'rock')
+        && (computerSelection === 'scissors')
+    ) {
+        return 'You win! Rock beats scissors!';
+    } else if (
+        (playerSelection === 'scissors')
+        && (computerSelection === 'rock')
+    ) {
+        return 'You lose! Rock beats scissors!';
+    } else if (
+        (playerSelection === 'scissors')
+        && (computerSelection === 'paper')
+    ) {
+        return 'You win! Paper beats scissors!';
+    } else {
+        return 'You tied. Play again!';
+    }
+}
+
+let playerSelection = 'paper';
+let computerSelection = computerPlay();
+
+console.log(playRound(playerSelection, computerSelection));
